3577a97ca7e23c8905d5a292bb3f3adeaf1f7a9f
Andrew Rock <Andrew Rock>
2017-05-24 16:03:29 -0500

    completed aip query function

--- js/api-config.js
+++ js/api-config.js
@@ -8,8 +8,20 @@ var config = {
 	databaseURL: apiData.databaseURL
 };
 
-api.getAPIsettings = function() {
+let getAPIsettings = function() {
 	return config;
 };
 
-module.exports = {getAPIsettings};
+let search = function(value) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: `${config.databaseURL}/3/search/movie?api_key=${config.apiKey}&language=en-US&query=${value}&page=1&include_adult=false`
+		}).done(function(data){
+			resolve(data);
+		}).fail(function(error){
+			reject(error);
+		})
+	});
+};
+
+module.exports = {getAPIsettings, search};
---
+++ js/cast.js
@@ -0,0 +1,26 @@
+// takes movie id and calls api for cast member of that movie
+
+"use strict";
+
+let api = require('./api-config.js');
+let castList = [];
+
+
+let getCast = function(movieID) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: `${api.databaseURL}/3/movie/${movieID}/credits?api_key=${api.apiKey}`
+		}).done(function(data){
+			for(let n=0; n<4; n+=1){
+				castList.push(data.cast[n].name);
+			}
+			resolve(castList);
+		}).fail(function(error) {
+			reject(error);
+		});
+	});
+};
+
+
+
+module.exports = {getCast};
