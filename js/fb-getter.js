4cc2efd07ceaf15deaf836fa2cdda8ee138b117b
Dylan Smith <jeffdylansmith@gmail.com>
2017-05-24 10:04:29 -0500

    fbgetter

---
+++ js/fb-getter.js
@@ -0,0 +1,9 @@
+"use strict";
+function getKey() {
+  return {
+    apiKey: "AIzaSyAp-43k9U4RE85cSIMv0ggMAbkDL6eBhAs",
+    authDomain: "movie-history-de5a5.firebaseapp.com",
+    databaseURL: "https://movie-history-de5a5.firebaseio.com/"
+  };
+}
+module.exports = getKey;
