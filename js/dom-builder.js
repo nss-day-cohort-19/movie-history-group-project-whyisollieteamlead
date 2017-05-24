ac46604ac1ae95f7a3b8550f074a29ed43ffae8d
ollieOsh <sosinusi@gmail.com>
2017-05-24 15:33:24 -0500

    dom-builder filepath and function call

--- js/dom-builder.js
+++ js/dom-builder.js
@@ -1,9 +1,9 @@
 "use strict";
 
 let $ = require('jquery'),
-    movieTemplate = require("./templates/movie_template.hbs");
+    movieTemplate = require("../templates/movie_template.hbs");
 
 function buildDom(movies) {
-	let masterCard = movie_template(movies);
+	let masterCard = movieTemplate(movies);
 	$("output").html(masterCard);
 }
