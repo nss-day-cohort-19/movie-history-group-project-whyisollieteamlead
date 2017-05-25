5f72462c19b1a4ece25163c7e7d4d39405a2e2cc
Chaz Henricks <chazhenricks@gmail.com>
2017-05-25 12:02:53 -0500

    COOL

---
+++ js/api-getter.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function getKey() {
+    return {
+        apiKey: "1deaf0c3830b051ec1a6416fb991af42",
+        authDomain: "",
+        databaseURL: "https://api.themoviedb.org"
+    };
+}
+
+module.exports = getKey;
