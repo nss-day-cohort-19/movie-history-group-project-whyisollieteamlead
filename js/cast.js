c19bae92f174b762980712e1eecd7285fd7214e0
Andrew Rock <Andrew Rock>
2017-05-25 09:59:42 -0500

    added search EL and made cast call append data to object

--- js/cast.js
+++ js/cast.js
@@ -3,18 +3,20 @@
 "use strict";
 
 let api = require('./api-config.js');
-let castList = [];
 
 
-let getCast = function(movieID) {
+
+let getCast = function(movieObj, movieID) {
 	return new Promise((resolve, reject) => {
 		$.ajax({
 			url: `${api.databaseURL}/3/movie/${movieID}/credits?api_key=${api.apiKey}`
 		}).done(function(data){
+			let castList = [];
 			for(let n=0; n<4; n+=1){
 				castList.push(data.cast[n].name);
 			}
-			resolve(castList);
+			movieObj.cast = castList;
+			resolve(movieObj);
 		}).fail(function(error) {
 			reject(error);
 		});
